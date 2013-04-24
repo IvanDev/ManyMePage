03f183d37b8d824a8da1559b5578bc65172096c6
Ivan Isaev <ivan.dev@gmail.com>
2013-04-24 16:36:15 +0700

    thai translation

--- js/locale-en.js
+++ js/locale-en.js
@@ -1,6 +1,8 @@
 oneLinerText = 'The app for taking pictures of many you!';
-appStoreAltText = 'Availabe on the App Store';
+appStoreAltText = 'Available on the App Store';
 getAppLabelText = 'Get ManyMe app for free!';
 videoAltText = 'Watch ManyMe in action';
 pageTitleText = 'ManyMe - an app for taking pictures of many you';
+
+//Do not translate
 appStoreImgFileName = 'appstore.png';
---
+++ js/locale-th.js
@@ -0,0 +1,8 @@
+oneLinerText = 'แอพสำหรับถ่ายรูปของคุณทั้งหลาย!';
+appStoreAltText = 'มีให้โหลดบน App Store';
+getAppLabelText = 'รับแอพ ManyMe ฟรี!';
+videoAltText = 'ดู ManyMe ในแบบดำเนินการ';
+pageTitleText = 'ManyMe - แอพสำหรับถ่ายรูปในหลายแอคของคุณ';
+
+//Do not translate
+appStoreImgFileName = 'appstore_th.svg';
