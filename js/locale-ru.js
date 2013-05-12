cabb39cc6a925b6aa757d391b284f13034afa684
Ivan Isaev <ivan.dev@gmail.com>
2013-05-12 13:44:15 +0700

    russian slogan fix

--- js/locale-ru.js
+++ js/locale-ru.js
@@ -1,6 +1,6 @@
-oneLinerText = 'Приложение для создания коллажей со многими Вами';
+oneLinerText = 'Cыграйте несколько ролей на одной сцене';
 appStoreAltText = 'Доступно App Store';
 getAppLabelText = 'Попробуйте ManyMe<br>бесплатно!';
 videoAltText = 'Посмотрите ManyMe в действии';
-pageTitleText = 'ManyMe - приложение для создания коллажей со многими Вами';
+pageTitleText = 'ManyMe - сыграйте несколько ролей на одной сцене';
 appStoreImgFileName = 'appstore_ru.png';
