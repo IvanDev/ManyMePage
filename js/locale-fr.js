5f821f4a47d0c02de312aebd3d998fc476d44a88
Ivan Isaev <ivan.dev@gmail.com>
2013-05-13 21:59:27 +0700

    privacy policy

---
+++ js/locale-fr.js
@@ -0,0 +1,8 @@
+oneLinerText = 'L’appli pour prendre des photos de beaucoup de vous !';
+appStoreAltText = 'Disponible sur l’App Store';
+getAppLabelText = 'Obtenez l’appli ManyMe app gratuitement !';
+videoAltText = 'Voir ManyMe en action';
+pageTitleText = 'ManyMe - une appli pour prendre des photos de beaucoup de vous';
+
+//Do not translate
+appStoreImgFileName = 'appstore_fr.svg';
